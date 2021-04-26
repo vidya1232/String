3f3e00781ce2892aa313ff14fdc93a5c53ecf61b
vidya <vidyamurugan1818gmail.com>
2021-04-26 20:42:02 +0530

    String

---
+++ Learning.java
@@ -0,0 +1,15 @@
+class Learning
+{
+ public static void main(String[] args)
+{
+System.out.println(args.length);
+System.out.println(args[0]);
+//Learning sl =new Learning();
+//int[] a ={10,20,30};
+//sl.add(a);
+}
+public void add(int[] ar)
+{
+System.out.println(ar.length);	
+}
+}
